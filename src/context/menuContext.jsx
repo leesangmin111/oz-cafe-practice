0349da80f43daee67d6d9601807063f3e8dea205
isangmin111 <sangmin9800@naver.com>
2025-02-27 15:12:02 +0900

    context apl리팩토링

---
+++ src/context/menuContext.jsx
@@ -0,0 +1,15 @@
+import { Children, createContext, useContext } from "react";
+import data from "../assets/data";
+
+const menuContext = createContext()
+
+ export function MenuProvider({Children}) {
+    return (
+        <menuContext.Provider value={{menu: data.menu}}>
+            {Children}
+        </menuContext.Provider>
+    )
+}
+export function useMenu() {
+return useContext(menuContext)
+}    
